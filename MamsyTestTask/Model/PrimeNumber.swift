502e5a04606f67b83f68c558f8b2e65f9831f788
Konstantin <n1kron92@gmail.com>
2018-07-23 20:33:39 +0300

    upload

---
+++ MamsyTestTask/Model/PrimeNumber.swift
@@ -0,0 +1,20 @@
+//
+//  PrimeNumber.swift
+//  MamsyTestTask
+//
+//  Created by  Kostantin Zarubin on 21.07.2018.
+//  Copyright © 2018  Kostantin Zarubin. All rights reserved.
+//
+
+import Foundation
+
+class PrimeNumber {
+    
+    var maxNumber: Int
+    var numbersList: [Int]
+    
+    init(maxNumber:Int, numbersList:[Int]) {
+        self.maxNumber = maxNumber
+        self.numbersList = numbersList
+    }
+}
